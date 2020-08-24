4c12a712530feaa7ad8aa42f2e46d9b8256612fb
felix kasyoki mutua <fkmutua@safaricom.co.ke>
2020-08-24 14:32:09 -0700

    added css

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 // Import express package
 const express = require('express');
 
+
 // Initialize express
 const app = express();
 
-// route for the index page
-app.get('/', (req, res) => {
-    response.send('<h1> Welcome to my app</h1>')
-});
+// Set a static folder
+app.use(express.static('public'));
 
 // Define the port number
-const PORT = 8000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
