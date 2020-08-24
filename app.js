dfb68a05457b188fd3a86a1e6ed405b163eed26a
felix kasyoki mutua <fkmutua@safaricom.co.ke>
2020-08-24 14:00:57 -0700

    added files

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import express package
+const express = require('express');
+
+// Initialize express
+const app = express();
+
+// route for the index page
+app.get('/', (req, res) => {
+    response.send('<h1> Welcome to my app</h1>')
+});
+
+// Define the port number
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
